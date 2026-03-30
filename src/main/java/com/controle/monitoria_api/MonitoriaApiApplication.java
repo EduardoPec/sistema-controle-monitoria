38d02b8f1e1e6419a9ac65e5c449d37ba806a62e
EduardoPec <eduardopecanha05@gmail.com>
2026-03-30 14:38:36 -0300

    feat: iniciando projeto spring boot

---
+++ src/main/java/com/controle/monitoria_api/MonitoriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.controle.monitoria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoriaApiApplication.class, args);
+	}
+
+}
