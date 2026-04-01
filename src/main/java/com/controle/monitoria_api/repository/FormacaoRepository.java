59adf56b9543a403702bccfe4eb455eff88abb45
Bernardo Megale Amorim <bernardomegale.amorim@ucsal.edu.br>
2026-03-31 23:51:40 -0300

    feat: add FormacaoRepository

---
+++ src/main/java/com/controle/monitoria_api/repository/FormacaoRepository.java
@@ -0,0 +1,17 @@
+package com.controle.monitoria_api.repository;
+
+import com.controle.monitoria_api.model.Formacao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormacaoRepository extends JpaRepository<Formacao, Long> {
+
+    Page<Formacao> findByProfessorId(Long professorId, Pageable paginacao);
+
+    boolean existsByProfessorIdAndTitulacaoAndNomeCursoAndInstituicao(
+            Long professorId, String titulacao, String nomeCurso, String instituicao);
+
+    boolean existsByProfessorIdAndTitulacaoAndNomeCursoAndInstituicaoAndIdNot(
+            Long professorId, String titulacao, String nomeCurso, String instituicao, Long id);
+}
